e07d79e413bade0117e338a04c6b1f7139d0cdc5
Artur Miłkowski <artur-milkowski@tlen.pl>
2022-12-12 13:03:24 +0100

    Install Vuestic UI framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
-
+import { createVuestic } from "vuestic-ui";
 import "./assets/main.css";
+import "vuestic-ui/css";
 
 const app = createApp(App);
-
 app.use(createPinia());
 app.use(router);
-
+app.use(createVuestic());
 app.mount("#app");
