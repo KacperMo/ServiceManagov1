a929c9ec978d195cd5c30bb0dcaebd9403756855
Artur Miłkowski <artur-milkowski@tlen.pl>
2022-12-12 16:19:28 +0100

    Add the sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,94 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import { RouterLink, RouterView, useRoute } from "vue-router";
+
+const activeRouteName = ref("home");
+const route = useRoute();
+// const items = [
+//   { name: "home" },
+//   { name: "user" },
+//   { name: "company" },
+//   { name: "about" },
+// ];
+const isRouteActive = (route) => {
+  console.log("isRouteActive", route);
+  return activeRouteName.value === route;
+};
+
+const setRouteActive = (route) => {
+  console.log("setRouteActive", route);
+  activeRouteName.value = route;
+};
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/about">About</RouterLink> |
-    <RouterLink :to="{ name: 'user' }">Users</RouterLink> |
-    <RouterLink :to="{ name: 'company' }">Companies</RouterLink>
-  </nav>
-  <RouterView v-slot="{ Component }">
-    <template v-if="Component">
-      <Suspense>
-        <component :is="Component"></component>
-        <template #fallback>Loading...</template>
-      </Suspense>
-    </template>
-  </RouterView>
+  <div class="row">
+    <div class="flex">
+      <div class="item">
+        <va-sidebar-item
+          :active="isRouteActive('home')"
+          @click="setRouteActive('home')"
+        >
+          <va-sidebar-item-content>
+            <va-icon name="dashboard" />
+            <va-sidebar-item-title>
+              <RouterLink to="/" class="va-link">Home</RouterLink>
+            </va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </va-sidebar-item>
+
+        <va-sidebar-item
+          :active="isRouteActive('about')"
+          @click="setRouteActive('about')"
+        >
+          <va-sidebar-item-content>
+            <va-icon name="room" />
+            <va-sidebar-item-title>
+              <RouterLink to="/about" class="va-link">About</RouterLink>
+            </va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </va-sidebar-item>
+
+        <va-sidebar-item
+          :active="isRouteActive('user')"
+          @click="setRouteActive('user')"
+        >
+          <va-sidebar-item-content>
+            <va-icon name="person" />
+            <va-sidebar-item-title>
+              <RouterLink :to="{ name: 'user' }" class="va-link"
+                >Users</RouterLink
+              >
+            </va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </va-sidebar-item>
+
+        <va-sidebar-item
+          :active="isRouteActive('company')"
+          @click="setRouteActive('company')"
+        >
+          <va-sidebar-item-content>
+            <va-icon name="apartment" />
+            <va-sidebar-item-title>
+              <RouterLink :to="{ name: 'company' }" class="va-link"
+                >Companies</RouterLink
+              >
+            </va-sidebar-item-title>
+          </va-sidebar-item-content>
+        </va-sidebar-item>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="item">
+        <RouterView v-slot="{ Component }">
+          <template v-if="Component">
+            <Suspense>
+              <component :is="Component"></component>
+              <template #fallback>Loading...</template>
+            </Suspense>
+          </template>
+        </RouterView>
+      </div>
+    </div>
+  </div>
 </template>
