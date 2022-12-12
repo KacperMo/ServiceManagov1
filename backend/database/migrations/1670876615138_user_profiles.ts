055f309318978b8f5af289516010a60920f1c46a
Kaco <kacperrtv@gmail.com>
2022-12-12 23:43:26 +0100

    Create Companies, afiliations, user_profile migration Create Afiliations, company, UserProfile model (empty )

---
+++ backend/database/migrations/1670876615138_user_profiles.ts
@@ -0,0 +1,31 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'user_profiles'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('user_id')      
+      table.string('position', 25)
+      table.string('name', 25).notNullable()
+      table.string('surname', 25)
+      table.string('phone_number', 25).notNullable()
+      table.string('email', 25).notNullable()
+      table.integer('salary', 5)
+      table.string('note', 255)
+      table.boolean('is_active', 1).notNullable()
+      table.string('avatar', 255)
+      table.string('baner', 255)
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.datetime('created_at', { useTz: true })
+      table.datetime('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ backend/database/migrations/1670877619943_affiliations.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'affiliations'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unique()
+      table.integer('user_id').unique()
+      table.integer('percentage_value').nullable()
+      table.int('afiliated_user_id').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.datetime('created_at', { useTz: true })
+      table.datetime('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
