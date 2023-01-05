a03bdad3cb5dd7e686412db68dc1c5d2ec8d3349
Kaco <kacperrtv@gmail.com>
2023-01-05 21:16:02 +0100

    Controllers CRUD fill up

--- backend/app/Controllers/Http/User/UserProfilesController.ts
+++ backend/app/Controllers/Http/User/UserProfilesController.ts
@@ -1,17 +1,46 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UserProfile from 'App/Models/UserProfile'
 
 export default class UserProfilesController {
-  public async index({}: HttpContextContract) {}
+  public async index ({}: HttpContextContract) {
+    return UserProfile.all()
+  }
 
-  public async create({}: HttpContextContract) {}
+  public async store ({request, response}: HttpContextContract) {
+    const position = request.input('position')
+    const name = request.input('name')
+    const surname = request.input('surname')
+    const phone = request.input('phone')
+    const email = request.input('email')
+    const salary = request.input('salary')
+    const note = request.input('note')
+    const avatar = request.input('avatar')
+    const baner = request.input('avatar')
 
-  public async store({}: HttpContextContract) {}
+    const profile = await UserProfile.create({
+      position: position,
+      name: name,
+      surname: surname,
+      phone: phone,
+      email: email,
+      salary: salary,
+      note: note,
+      avatar: avatar,
+      baner: baner,
+    })
+    response.status(201)
+    return response.created({ data: profile })
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show ({params}: HttpContextContract) {
+    const profile = await UserProfile.find(params.id)
 
-  public async edit({}: HttpContextContract) {}
+    return profile
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async edit ({}: HttpContextContract) {}
 
-  public async destroy({}: HttpContextContract) {}
+  public async update ({}: HttpContextContract) {}
+
+  public async destroy ({}: HttpContextContract) {}
 }
