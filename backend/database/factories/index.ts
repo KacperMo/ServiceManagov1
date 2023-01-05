0513dfb3632e91578f4352278f5f5183ffb88983
Kaco <kacperrtv@gmail.com>
2023-01-05 12:38:06 +0100

    Add UserProfile, Affiliation and Supplier Factory

--- backend/database/factories/index.ts
+++ backend/database/factories/index.ts
@@ -2,6 +2,9 @@ import Factory from '@ioc:Adonis/Lucid/Factory'
 import User from 'App/Models/User'
 import Company from 'App/Models/Company'
 import ProductImg from 'App/Models/ProductImg'
+import UserProfile from 'App/Models/UserProfile'
+import Affiliation from 'App/Models/Affiliation'
+import Supplier from 'App/Models/Supplier'
 
 export const UserFactory = Factory.define(User, ({ faker }) => {
   return {
@@ -18,7 +21,7 @@ export const CompanyFactory = Factory.define(Company, ({ faker }) => {
     owner_id: faker.datatype.number(),
     city: faker.address.cityName(),
     phone_number: faker.phone.number('###-###-###'),
-    NIP: faker.lorem.text(),
+    nip: faker.lorem.text(),
   }
 }).build()
 
@@ -31,4 +34,30 @@ export const ProductImgFactory = Factory.define(ProductImg, ({ faker }) => {
     img_src4: faker.system.commonFileName('jpg'),
   }
 }).build()
+
+export const UserProfileFactory = Factory.define(UserProfile, ({ faker }) => {
+  return {
+    position: faker.providers.job(),
+    name: faker.name.firstName(),
+    surname: faker.system.lastName('jpg'),
+    phone: faker.phone.number('###-###-###'),
+    email: faker.internet.exampleEmail(),
+  }
+}).build()
+
+export const AffiliationFactory = Factory.define(Affiliation, ({ faker }) => {
+  return {
+    percentage_value: faker.datatype.number(20)
+  }
+}).build()
+
+export const SupplierFactory = Factory.define(Supplier, ({ faker }) => {
+  return {
+    company_name: faker.company.name(),
+    nip: nip: faker.lorem.text(),
+    api: faker.internet.url(),
+    api_key: faker.internet.password(20),
+  }
+}).build()
+
 
