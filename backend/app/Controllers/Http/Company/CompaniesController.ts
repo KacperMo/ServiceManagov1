dc4bc6e7a6056679252b38aba8dca470ee533032
Artur Miłkowski <artur-milkowski@tlen.pl>
2023-01-23 17:54:57 +0100

    Add the create company validator

--- backend/app/Controllers/Http/Company/CompaniesController.ts
+++ backend/app/Controllers/Http/Company/CompaniesController.ts
@@ -1,26 +1,31 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Company from 'App/Models/Company'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Company from "App/Models/Company";
+import CreateCompanyValidator from "App/Validators/CreateCompanyValidator";
 
 export default class CompaniesController {
-  public async index ({}: HttpContextContract) {
-    return Company.all()
+  public async index({}: HttpContextContract) {
+    return Company.all();
   }
 
-  public async store ({request, response}: HttpContextContract) {
-    const name = request.input('name')
-    const industry_id = request.input('industry_id')
-    const owner_id = request.input('owner_id')
-    const city = request.input('city')
-    const street = request.input('street')
-    const zip_code = request.input('zip_code')
-    const house_number = request.input('house_number')
-    const apartment_number = request.input('apartment_number')
-    const nip = request.input('nip')
-    const phone_number = request.input('phone_number')
-    const email = request.input('email')
-    const logo = request.input('logo')
-
+  public async store({ request, response }: HttpContextContract) {
+    /*
+    const name = request.input("name");
+    const industry_id = request.input("industry_id");
+    const owner_id = request.input("owner_id");
+    const city = request.input("city");
+    const street = request.input("street");
+    const zip_code = request.input("zip_code");
+    const house_number = request.input("house_number");
+    const apartment_number = request.input("apartment_number");
+    const nip = request.input("nip");
+    const phone_number = request.input("phone_number");
+    const email = request.input("email");
+    const logo = request.input("logo");
+    */
+    const payload = await request.validate(CreateCompanyValidator);
+    const company = await Company.create(payload);
+    /*
     const company = await Company.create({
       name: name,
       industry_id: industry_id,
@@ -34,34 +39,47 @@ export default class CompaniesController {
       phone_number: phone_number,
       email: email,
       logo: logo,
-    })
-
-    return response.created({ data: company })
+    });
+    */
+    return response.created({ data: company });
   }
 
   //store vs create
   /*   public async create({}: HttpContextContract) {}
- */
-  public async show ({params}: HttpContextContract) {
-    const company = await Company.find(params.id)
+   */
+  public async show({ params }: HttpContextContract) {
+    const company = await Company.find(params.id);
 
-    return company
+    return company;
   }
 
-  public async update ({request, params}: HttpContextContract) {
-    const company = await Company.findOrFail(params.id)
-    company.merge(request.only([
-      'name', 'email', 'phone', 'city', 'street', 'zip_code',
-      'house_number', 'apartment_number', 'nip', 'phone_number',
-      'email', 'logo', 'rememberMeToken']))
-    await company.save()
-    return company
+  public async update({ request, params }: HttpContextContract) {
+    const company = await Company.findOrFail(params.id);
+    company.merge(
+      request.only([
+        "name",
+        "email",
+        "phone",
+        "city",
+        "street",
+        "zip_code",
+        "house_number",
+        "apartment_number",
+        "nip",
+        "phone_number",
+        "email",
+        "logo",
+        "rememberMeToken",
+      ])
+    );
+    await company.save();
+    return company;
   }
 
-  public async destroy ({response, params}: HttpContextContract) {
-    const company = await Company.findOrFail(params.id)
-    await company.delete()
+  public async destroy({ response, params }: HttpContextContract) {
+    const company = await Company.findOrFail(params.id);
+    await company.delete();
 
-    return response.noContent()
+    return response.noContent();
   }
 }
---
+++ backend/app/Validators/CreateCompanyValidator.ts
@@ -0,0 +1,23 @@
+import { schema, rules, CustomMessages } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class CreateCompanyValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    industry_id: schema.number(),
+    owner_id: schema.number(),
+    name: schema.string([rules.maxLength(255)]),
+    city: schema.string.optional([rules.maxLength(255)]),
+    street: schema.string.optional([rules.maxLength(255)]),
+    zip_code: schema.string.optional(),
+    house_number: schema.string.optional(),
+    apartment_number: schema.string.optional(),
+    nip: schema.string.optional(),
+    phone_number: schema.string.optional(),
+    email: schema.string.optional([rules.email()]),
+    logo: schema.string.optional(),
+  });
+
+  public messages: CustomMessages = {};
+}
