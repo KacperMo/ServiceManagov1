868ab14aa62ca6b098531cf29873aff62bf5c051
KacperMo <kacperrtv@gmail.com>
2023-01-25 20:52:43 +0100

    Add Customer Test

---
+++ backend/tests/functional/customer.spec.ts
@@ -0,0 +1,62 @@
+import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
+import { CustomerFactory } from 'Database/factories'
+
+test.group('Customer', () => {
+  test('index', async ({ }) => {
+    const user = await UserFactory.create()
+    const response = await client.get('/imgs_collection').loginAs(user)
+
+    response.assertStatus(200)
+  })
+  
+  test('unauthorized_index', async ({ client }) => {
+    const response = await client.get('/imgs_collection')
+
+    response.assertStatus(401)
+  })
+
+  test('store', async ({ client }) => {
+    const user = await UserFactory.create()
+    const customer = await CustomerFactory.make()
+    const response = await client.post('/customers').json({
+      customer_id: 1,
+      name: customer.name,
+      rating: customer.rating,
+      street: customer.street,
+      nip: customer.nip,
+      company_name: customer.company_name,
+    }).loginAs(user)
+    
+      response.assertStatus(201)
+    })
+
+    test('show', async ({ client }) => {
+      const user = await UserFactory.create()
+      const customer = await CustomerFactory.create()
+      const response = await client.get(`/customers/${customer.id}`).loginAs(user)
+  
+      response.assertStatus(200)
+    })
+
+    test('update', async ({ client }) => {
+      const user = await UserFactory.create()
+      const customer = await CustomerFactory.create()
+      const customer1 = await CustomerFactory.make()
+      const response = await client
+        .put(`/customers/${customer.id}`)
+        .json({ rating: customer1.rating })
+        .loginAs(user)
+  
+      response.assertStatus(200)
+      response.assertBodyContains({ rating: customer1.rating })
+    })
+  
+    test('destroy', async ({ client }) => {
+      const user = await UserFactory.create()
+      const customer = await CustomerFactory.create()
+      const response = await client.delete(`/customers/${customer.id}`).loginAs(user)
+  
+      response.assertStatus(204)
+    })
+})
