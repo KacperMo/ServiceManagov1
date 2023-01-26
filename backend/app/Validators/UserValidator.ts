45d3bab47a7fd8d7977056d7c8c13d0dabc1c385
KacperMo <kacperrtv@gmail.com>
2023-01-27 00:01:20 +0100

    Add User Validator

---
+++ backend/app/Validators/UserValidator.ts
@@ -0,0 +1,14 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UserValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string([rules.email()]),
+    password: schema.string(),
+    name: schema.string([rules.maxLength(255)]),
+  });
+
+  public messages: CustomMessages = {'ok.'}
+}
