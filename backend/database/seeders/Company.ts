1eb14297e49b6d05f3642057b73bdc2c3494153f
Artur Miłkowski <artur-milkowski@tlen.pl>
2023-01-29 11:55:48 +0100

    Add the right path to import in the company seeder

--- backend/database/seeders/Company.ts
+++ backend/database/seeders/Company.ts
@@ -1,5 +1,5 @@
 import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
-import Company from "App/Models/Company";
+import Company from "App/Models/Company/Company";
 
 export default class extends BaseSeeder {
   public static environment = ["development", "testing"];
--- backend/database/seeders/MainSeeder/Index.ts
+++ backend/database/seeders/MainSeeder/Index.ts
@@ -1,23 +1,23 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { UserFactory } from 'Database/factories'
-import { CompanyFactory } from 'Database/factories'
-import { ProductImgFactory } from 'Database/factories'
-import { IndustryFactory } from 'Database/factories'
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import { UserFactory } from "Database/factories";
+import { CompanyFactory } from "Database/factories";
+import { ProductImgFactory } from "Database/factories";
+import { IndustryFactory } from "Database/factories";
 
 export default class extends BaseSeeder {
   private async runSeeder(Seeder: { default: typeof BaseSeeder }) {
     await new Seeder.default(this.client).run();
   }
 
-  public async run () {
+  public async run() {
     //await IndustryFactory.createMany(5)
     //await CompanyFactory.createMany(10)
     //await ProductImgFactory.createMany(10)
     //await UserFactory.createMany(10)
 
-    await this.runSeeder(await import('../Industry'))
-    await this.runSeeder(await import('../Company'))
-    await this.runSeeder(await import('../ProductImg'))
-    await this.runSeeder(await import('../User'))
+    await this.runSeeder(await import("../Industry"));
+    await this.runSeeder(await import("../Company"));
+    await this.runSeeder(await import("../ProductImg"));
+    await this.runSeeder(await import("../User"));
   }
 }
