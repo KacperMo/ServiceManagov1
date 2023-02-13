eefa9c9cacd1d17c95839c266633349d3235c49a
Artur M <artur-milkowski@tlen.pl>
2023-02-13 10:49:27 +0100

    Preload industry in show category method

--- backend/app/Controllers/Http/Company/CategoriesController.ts
+++ backend/app/Controllers/Http/Company/CategoriesController.ts
@@ -3,24 +3,33 @@ import Category from "App/Models/Company/Category";
 import CreateCategory from "App/Validators/Company/CreateCategoryValidator";
 
 export default class CategoriesController {
-  public async index({}: HttpContextContract) {
+  public async index({}: HttpContextContract): Promise<Category[]> {
     return Category.query().orderBy("name").preload("industry");
   }
 
-  public async store({ request, response }: HttpContextContract) {
+  public async store({
+    request,
+    response,
+  }: HttpContextContract): Promise<void> {
     const payload = await request.validate(CreateCategory);
     const category = await Category.create(payload);
 
     return response.created(category);
   }
 
-  public async show({ params }: HttpContextContract) {
-    const category = Category.findOrFail(params.id);
+  public async show({ params }: HttpContextContract): Promise<Category> {
+    const category = Category.query()
+      .where("id", "=", params.id)
+      .preload("industry")
+      .firstOrFail();
 
     return category;
   }
 
-  public async update({ request, params }: HttpContextContract) {
+  public async update({
+    request,
+    params,
+  }: HttpContextContract): Promise<Category> {
     const category = await Category.findOrFail(params.id);
     const payload = await request.validate(CreateCategory);
     category.merge(payload).save();
@@ -28,7 +37,10 @@ export default class CategoriesController {
     return category;
   }
 
-  public async destroy({ response, params }: HttpContextContract) {
+  public async destroy({
+    response,
+    params,
+  }: HttpContextContract): Promise<void> {
     const category = await Category.findOrFail(params.id);
     await category.delete();
 
